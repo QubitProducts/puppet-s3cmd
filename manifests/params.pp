94cd27b9032240f5b4b1e86142f947972060d760
Rednut Infomatics <dotcomstu@gmail.com>
2015-09-28 20:19:07 +0100

    Update params.pp
    make work with non debian distros

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,7 @@
 class s3cmd::params {
 
-case $::lsbdistid {
-    'Ubuntu','Debian' : {
-        $packages = ['s3cmd','python-magic','python-central']
-    }
-    default: { fail("Unsupported OS: ${::lsbdistid}") }
- }
+
+    $packages = ['s3cmd','python-magic']
+    # ,'python-central'
+
 }
