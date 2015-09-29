e01403af3b7d5b12886d2889d95bcb45bb724157
Rednut Infomatics <dotcomstu@gmail.com>
2015-09-29 13:35:45 +0100

    fix s3cmd to work with debian and redhats

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,17 @@
 class s3cmd::params {
 
+  case $::operatingsystem {
+    'Ubuntu', 'Debian' : {
+      $packages = ['s3cmd','python-magic', 'python-central' ]
+    }
 
-    $packages = ['s3cmd','python-magic']
-    # ,'python-central'
+    'CentOS', 'Redhat': {
+      $packages = [ 's3cmd','python-magic']
+    }
+
+    default: {
+      fail("Unsupported OS: ${::operatingsystem} ")
+    }
+
 
 }
